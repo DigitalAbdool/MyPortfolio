02d2ee5882c00fb9b39a7ac5359b18354882c466
Aldosary <abdulazizkabir001@gmail.com>
2024-05-12 10:40:34 +0100

    created the contact and footer of the page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <footer className="footer">
+        <p>
+          &copy; 2024. {" "}
+          <a href="https://github.com/DigitalAbdool">Abdulazeez</a>.
+        </p>
+      </footer>
+    );
+  };
+  export default Footer;
